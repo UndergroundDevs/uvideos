b0e1bbdef4f5cd9f64bf181e5796bf278713a38d
Kevson Filipe <kevsonfilipesantos@gmail.com>
2024-02-10 21:09:29 -0300

    feat: reordering folders

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ffm*s!ivtb!&g2w=osnif_!^9f_1y%&jz4$p))&8kzp@y1onvc'
+SECRET_KEY = 'django-insecure-a+dvoitu6swmxdr08-_)3p4w)&q5-7fl=+zn1$5n7-x24qhpt)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
